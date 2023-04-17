657e888c02712a0efd5f74f88c174bbb2f3a5209
rashi <eynorashi@gmail.com>
2023-04-17 11:55:35 +0530

    reupload

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -14,39 +14,40 @@ function NavBar() {
           <NavLink exact to="/" className="nav-logo">
             <img src={Logomain} alt="" />
           </NavLink>
+          <div className="nav-menu-wrapper">
+              <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className="nav-item">
+                  <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact to="/about" activeClassName="active" className="nav-links" onClick={handleClick}>
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact to="/services" activeClassName="active" className="nav-links" onClick={handleClick}>
+                    Services
+                  </NavLink>
+                </li>
 
-          <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <NavLink exact to="/" activeClassName="active" className="nav-links" onClick={handleClick}>
-                 Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink exact to="/about" activeClassName="active" className="nav-links" onClick={handleClick}>
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink exact to="/services" activeClassName="active" className="nav-links" onClick={handleClick}>
-                Services
-              </NavLink>
-            </li>
+                <li className="nav-item">
+                  <NavLink exact to="/portfolio" activeClassName="active" className="nav-links" onClick={handleClick}>
+                    Case Study
+                  </NavLink>
+                </li>
 
-            <li className="nav-item">
-              <NavLink exact to="/portfolio" activeClassName="active" className="nav-links" onClick={handleClick}>
-                Case Study
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink exact to="/contact" activeClassName="active" className="nav-links" onClick={handleClick}>
-                Contact Us
-              </NavLink>
-            </li>
-          </ul>
-           <div className="quote-btn">           
-            <a href={url}>Get A Quote</a>       
-          </div> 
+                <li className="nav-item">
+                  <NavLink exact to="/contact" activeClassName="active" className="nav-links" onClick={handleClick}>
+                    Contact Us
+                  </NavLink>
+                </li>
+              </ul>
+              <div className="quote-btn">           
+                <a href={url}>Get A Quote</a>       
+              </div> 
+          </div>
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
